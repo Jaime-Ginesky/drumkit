7a41071c0841cf75e9d0bc9a4646f44a09b878fa
Jaime-Ginesky <jaimelee79@gmail.com>
2024-02-25 20:49:16 -0800

    created react app/removed unneccasry code/keboard component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Keyboard from "./components/Keyboard/Keyboard";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Keyboard />
+      Beat Box!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,7 @@
+
+
+const Keyboard = () => {
+
+}
+
+export default Keyboard;
